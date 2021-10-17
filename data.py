8423e96e2d85cf797dc6311f023fddf5328efb70
Chris-On-PC <partner.christiaan.viviers@philips.com>
2021-10-17 23:30:12 +0200

    added sub file loading

--- data.py
+++ data.py
@@ -6,14 +6,20 @@ import numpy as np
 import cv2
 
 class XrayImageDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, transform = None, patch_size: int = 128, overlap: float = 0.25):
-        #super(XrayImageDataset, self).__init__(img_dir, transform=transform, patch_size = patch_size, overlap = overlap)
+    def __init__(self, img_dir, transform = None, patch_size: int = 128, overlap: float = 0.25, incldude_subdirs = True):
 
         self.img_dir = img_dir
         self.transform = transform
         self.overlap = overlap
         self.patch_size = patch_size
-        self.filenames = os.listdir(self.img_dir)
+        self.filenames = list()
+
+        if incldude_subdirs:
+            for (dirpath, dirnames, filenames) in os.walk(img_dir):
+                # for dirname in dirnames:
+                self.filenames += [os.path.join(dirpath.replace(img_dir, '')[1:], file) for file in filenames]
+        else:
+            self.filenames = os.listdir(self.img_dir)
 
     def __len__(self):
         return len(self.filenames)
@@ -21,8 +27,8 @@ class XrayImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
 
         image = cv2.imread(os.path.join(self.img_dir, self.filenames[idx]), cv2.IMREAD_GRAYSCALE)
+        
         patches = self.split_image(image)
-        print(patches.shape)
         if self.transform:
             patches = self.transform(patches)
 
@@ -41,7 +47,7 @@ class XrayImageDataset(torch.utils.data.Dataset):
             for j in X_points:
                 split = image[i:i+self.patch_size, j:j+self.patch_size]
                 patches[count, :, :] = split
-                cv2.imwrite('patch_{}.{}'.format( count, frmt), split)
+                #cv2.imwrite('patch_{}.{}'.format( count, frmt), split)
                 count += 1
         return patches
 
