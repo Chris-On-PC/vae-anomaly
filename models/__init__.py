80bdb4d10cf773bb9b6b5449caf931afec2f834c
Chris-On-PC <partner.christiaan.viviers@philips.com>
2021-10-18 16:35:50 +0200

    refactor

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,10 @@
 from .base import *
 from .vanillaVae import *
+
+# Aliases
+VAE = VanillaVAE
+GaussianVAE = VanillaVAE
+# CVAE = ConditionalVAE
+# GumbelVAE = CategoricalVAE
+
+vae_models = {'VanillaVAE':VanillaVAE}
